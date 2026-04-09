5cea4d7dc7673fbef5c96d4cf8a0fb0fa065ae47
cijenwasgone <mrfaris404@gmail.com>
2026-04-09 23:11:06 +0700

    Add files via upload
    Update Hari 2: Menambahkan fungsi (def) dan fitur simpan laporan ke file TXT

--- warung.py
+++ warung.py
@@ -1,9 +1,42 @@
+semua_pembeli = []
+semua_total = []
+katalog = {
+    "Kopi" : 5000,
+    "Indomie" : 7500,
+    "Nutrisari" : 5000
+}
+
+def cetak_struk(nama_pembeli, total_bayar):
+    print("=====STRUK BELANJA====")
+    print("Nama Pelanggan : " + nama_pembeli)
+    print(f"Total yang harus dibayar : Rp {total_bayar}")
+    print("======================")
+    
+def simpan_laporan(daftar_nama,list_total):
+    with open ("laporan_hari_ini.txt","w") as teks:
+        teks.write("====Laporan Penjualan Hari Ini====\n")
+        
+        jumlah_orang={len(daftar_nama)}
+        uang_masuk={sum(list_total)}
+        
+        teks.write(f"Jumlah Pembeli Hari Ini : {jumlah_orang}\n")
+        teks.write(f"Total Uang Masuk Hari Ini : Rp {uang_masuk}\n")
+        
+        teks.write("Daftar Pelanggan:\n")
+        for nama in daftar_nama:
+            teks.write(f"- {nama}\n")
+    print(">> Laporan berhasil disimpan ke file 'laporan_warung.txt' <<")         
+
 lagi="y"
 while lagi == "y":
     print("TOKO CIJEN")
     nama=input("siapa nama kamu ? ")
-    harga_barang=int(input("berapa harga barang yang kamu beli ? "))
+    beli_barang=(input("Mau beli apa ? (Kopi/Indomie/Nutrisari) : "))
+    while beli_barang not in katalog:
+        print("Maaf barang tersebut tidak ada, coba perbaiki ketikanmu!")
+        beli_barang=(input("Mau beli apa ? (Kopi/Indomie/Nutrisari) : "))
     jumlah_barang=int(input("berapa jumlah barang yang kamu beli ? "))
+    harga_barang=katalog[beli_barang]
     total_harga=harga_barang*jumlah_barang
 
     if total_harga > 100000:
@@ -12,10 +45,11 @@ while lagi == "y":
     else: 
         print("maaf kamu tidak mendapatkan diskon")
         
-    print("=====STRUK BELANJA====")
-    print("Nama Pelanggan " + nama)
-    print(f"Total yang harus dibayar : Rp {total_harga}")
-    print("======================")
-    lagi = input("mau belanja lagi ? (y/n): ")
+    semua_pembeli.append(nama)    
+    semua_total.append(total_harga)    
     
+    cetak_struk(nama,total_harga)    
+    lagi = input("mau belanja lagi ? (y/n): ")
+
 print("toko ditutup,terimakasih!")
+simpan_laporan(semua_pembeli, semua_total)
